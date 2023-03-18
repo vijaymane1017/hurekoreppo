8f2de504ca0df9f7ca1dc741895a226a49efda19
vijaymane1017 <vijaymane1017@gmail.com>
2023-03-19 00:57:00 +0530

    changes made for hureko

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request,redirect
+import pickle
+import numpy as np
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+ 
+
+app.run(debug=True)
